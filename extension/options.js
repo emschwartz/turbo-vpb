7af56730d65de909ee1dfa09717048672773e487
Evan Schwartz <evan@ripple.com>
2020-07-25 13:21:45 -0400

    fix(extension): correctly read checkbox value

--- extension/options.js
+++ extension/options.js
@@ -6,14 +6,14 @@ document.getElementById('settings')
             messageTemplates.push({
                 label: document.getElementById('messageLabel1').value || 'First Message',
                 message: document.getElementById('messageText1').value,
-                result: document.getElementById('messageResultTexted1').value === 'on' ? 'Texted' : null
+                result: document.getElementById('messageResultTexted1').checked ? 'Texted' : null
             })
         }
         if (document.getElementById('messageText2').value) {
             messageTemplates.push({
                 label: document.getElementById('messageLabel2').value || 'Second Message',
                 message: document.getElementById('messageText2').value,
-                result: document.getElementById('messageResultTexted2').value === 'on' ? 'Texted' : null
+                result: document.getElementById('messageResultTexted2').checked ? 'Texted' : null
             })
         }
         browser.storage.local.set({
@@ -32,14 +32,14 @@ browser.storage.local.get(['yourName', 'messageTemplates', 'url'])
                 document.getElementById('messageLabel1').value = messageTemplates[0].label
                 document.getElementById('messageText1').value = messageTemplates[0].message
                 if (messageTemplates[0].result === 'Texted') {
-                    document.getElementById('messageResultTexted1').value = 'on'
+                    document.getElementById('messageResultTexted1').checked = true
                 }
             }
             if (messageTemplates && messageTemplates.length >= 2) {
                 document.getElementById('messageLabel2').value = messageTemplates[1].label
                 document.getElementById('messageText2').value = messageTemplates[1].message
                 if (messageTemplates[1].result === 'Texted') {
-                    document.getElementById('messageResultTexted2').value = 'on'
+                    document.getElementById('messageResultTexted2').checked = true
                 }
             }
         }
