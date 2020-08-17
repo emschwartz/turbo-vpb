ef43fad042719d335af98f91e1312e61754c6e06
Evan Schwartz <evan@ripple.com>
2020-08-17 10:16:09 -0400

    chore(connect) capture peerjs errors with sentry

--- docs/connect.js
+++ docs/connect.js
@@ -149,6 +149,8 @@ function connectPeer() {
         document.getElementById('name').innerText = ''
         document.getElementById('phoneNumber').href = ''
         document.getElementById('phoneNumber').innerText = ''
+
+        Sentry.captureException(err)
     })
     peer.once('open', () => {
         log('peer opened')
@@ -185,6 +187,7 @@ function establishConnection() {
         conn = null
 
         setStatus('Error. Reload Tab.', 'danger')
+        Sentry.captureException(err)
     })
     conn.once('close', () => {
         setStatus('Not Connected', 'danger')
