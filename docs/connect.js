a37f5e5b80096cf3d5dffc00be6c916d83eb1389
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2020-10-08 21:01:44 -0400

    fix(connect): start call timer on touchstart event
    fixes a bug where it was not correctly tracking calls placed with Google Voice

--- docs/connect.js
+++ docs/connect.js
@@ -147,9 +147,7 @@ if (sessionIsComplete()) {
         })
     document.getElementById('phoneNumber')
         .addEventListener('touchstart', () => {
-            if (window.localStorage.getItem('requireLongPressMode')) {
-                lastCallStartTime = Date.now()
-            }
+            lastCallStartTime = Date.now()
         })
 
     // Require long-press mode setting
