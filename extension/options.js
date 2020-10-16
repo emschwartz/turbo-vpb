184cbde213fb29dea99d1605a702bd6abfcdd454
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2020-10-16 18:32:36 -0400

    feat(extension): export settings from options page

--- extension/options.js
+++ extension/options.js
@@ -36,6 +36,8 @@ browser.storage.local.get(['yourName', 'messageTemplates'])
                 message: ''
             })
         }
+
+        setShareUrl(messageTemplates)
     })
 
 function addMessageTemplate(template) {
@@ -83,8 +85,22 @@ function saveSettings() {
         }
     }
 
+    setShareUrl(messageTemplates)
+
     return browser.storage.local.set({
         yourName: document.getElementById('yourName').value,
         messageTemplates
     })
+}
+
+function setShareUrl(messageTemplates) {
+    const shareButton = document.getElementById('share-settings')
+    if (shareButton) {
+        if (messageTemplates && messageTemplates.length > 0) {
+            shareButton.href = `https://turbovpb.com/share?messageTemplates=${encodeURIComponent(JSON.stringify(messageTemplates))}`
+            shareButton.classList.remove('disabled')
+        } else {
+            shareButton.classList.add('disabled')
+        }
+    }
 }
