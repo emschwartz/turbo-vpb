b316e91f1c44682c7488c5057ece0a30c3e69bba
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2020-10-26 12:44:55 -0400

    fix(connect): longer wait after page becomes visible
    before reconnecting or counting errors

--- docs/connect.js
+++ docs/connect.js
@@ -2,6 +2,7 @@
 
 console.log('Loading connect.js')
 const CONNECT_TIMEOUT = 30000
+const WAIT_AFTER_PAGE_BECOMES_VISIBLE = 100
 
 const TEXT_MESSAGE_MARK_TEXTED_ICON = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chat-text" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
@@ -274,7 +275,8 @@ async function start() {
             // Only show and report errors if they are not caused by the page being put to sleep by the browser
             // The error event fires after the document.readyState is changed to visible, which is why
             // we check if the page became visible within a certain number of milliseconds
-            if (document.visibilityState === 'hidden' || (Date.now() - pageLastBecameVisible) < 10) {
+            if (document.visibilityState === 'hidden'
+                || (Date.now() - pageLastBecameVisible) <= WAIT_AFTER_PAGE_BECOMES_VISIBLE) {
                 return
             }
 
@@ -363,7 +365,7 @@ async function start() {
 
             if (peerManager && !peerManager.isStopped()) {
                 // Wait in case other event handlers will be fired after this one
-                await new Promise((resolve) => setTimeout(resolve, 10))
+                await new Promise((resolve) => setTimeout(resolve, WAIT_AFTER_PAGE_BECOMES_VISIBLE))
 
                 // Make sure we're still connected
                 await peerManager.reconnect(null, true)
