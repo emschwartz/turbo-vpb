f5c3d085e678c05894033e70b4624ee311776d46
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2022-08-31 21:16:50 +0200

    Render pages as HTML and rearrange static assets

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,25 @@ use axum::body::Bytes;
 use axum::extract::ws::{Message, WebSocketUpgrade};
 use axum::extract::{Extension, Path};
 use axum::http::StatusCode;
-use axum::response::IntoResponse;
+use axum::response::{Html, IntoResponse};
 use axum::routing::{delete, get, get_service};
 use axum::{Router, Server};
 use dashmap::DashMap;
 use futures::{sink::SinkExt, stream::StreamExt};
-use serde::Deserialize;
+use handlebars::{no_escape, Handlebars};
+use serde::{Deserialize, Serialize};
+use std::path::Path as FilePath;
 use std::{error::Error, net::SocketAddr, sync::Arc, time::Duration};
+use tokio::fs::read_to_string;
 use tokio::select;
 use tokio::sync::broadcast::{channel, Sender};
 use tokio::time::sleep;
-use tower_http::services::ServeDir;
+use tower_http::services::{ServeDir, ServeFile};
+use tower_http::trace::TraceLayer;
 use tracing::{debug, info};
 
 const PING_INTERVAL: Duration = Duration::from_secs(20);
+const PAGES: &[&str] = &["index", "connect", "share"];
 
 type State = Arc<DashMap<String, Channel>>;
 struct Channel {
@@ -33,16 +38,42 @@ enum Identity {
     Website,
 }
 
+#[derive(Serialize)]
+struct TemplateParams {
+    content: String,
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+
+    let root_dir = FilePath::new(env!("CARGO_MANIFEST_DIR"));
+    let src_dir = root_dir.join("src");
+    let static_dir = src_dir.join("static");
+    let favicon_dir = static_dir.join("favicons");
+
+    let static_file_service = get_service(ServeDir::new(static_dir))
+        .fallback(get_service(ServeFile::new(favicon_dir.join("favicon.ico"))))
+        .handle_error(internal_service_error);
+
+    // Render pages
+    let app = api_router()
+        .merge(pages_router().await)
+        .fallback(static_file_service)
+        .layer(TraceLayer::new_for_http());
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    info!("listening on {}", addr);
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+fn api_router() -> Router {
     let state: State = Default::default();
 
-    let static_path = format!("{}/src/static", env!("CARGO_MANIFEST_DIR"));
-    let static_file_service =
-        get_service(ServeDir::new(static_path)).handle_error(internal_service_error);
-
-    let app = Router::new()
+    Router::new()
         .route(
             "/health",
             get(|| async { "Hello, I am the TurboVPB server\n" }),
@@ -52,15 +83,43 @@ async fn main() {
             get(ws_handler).post(post_channel),
         )
         .route("/c/:channel_id", delete(delete_channel))
-        .fallback(static_file_service)
-        .layer(Extension(state));
+        .layer(Extension(state))
+}
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
-    info!("listening on {}", addr);
-    Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+async fn pages_router() -> Router {
+    let root_dir = FilePath::new(env!("CARGO_MANIFEST_DIR"));
+    let src_dir = root_dir.join("src");
+    let pages_dir = src_dir.join("pages");
+
+    // Load template
+    let mut templates = Handlebars::new();
+    // Don't escape HTML because we are specifically using this to embed the page content as HTML in the template
+    templates.register_escape_fn(no_escape);
+    templates
+        .register_template_file("template", src_dir.join("template.hbs"))
+        .expect("Error parsing template");
+
+    let mut router = Router::new();
+
+    // Render each of the pages and add them as routes
+    for page in PAGES {
+        let path = pages_dir.join(format!("{}.html", page));
+        let content = read_to_string(path)
+            .await
+            .expect(&format!("Error reading page {}", page));
+        let content = templates
+            .render("template", &TemplateParams { content })
+            .expect(&format!("Error rendering page {}", page));
+        let service = get(move || async move { Html(content) });
+        if *page == "index" {
+            router = router.route("/", service.clone());
+        }
+        router = router
+            .route(&format!("/{}", page), service.clone())
+            .route(&format!("/{}.html", page), service);
+    }
+
+    router
 }
 
 async fn internal_service_error(_: impl Error) -> impl IntoResponse {
