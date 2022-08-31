be7808c89ebe0f2c0c22d7d3b487495808a0b6a0
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2022-08-31 22:01:55 +0200

    Instrument API

--- src/api.rs
+++ src/api.rs
@@ -1,17 +1,14 @@
-use axum::body::Bytes;
 use axum::extract::ws::{Message, WebSocketUpgrade};
 use axum::extract::{Extension, Path};
-use axum::response::IntoResponse;
 use axum::routing::{delete, get};
-use axum::Router;
+use axum::{body::Bytes, http::StatusCode, response::IntoResponse, Router};
 use dashmap::DashMap;
 use futures::{sink::SinkExt, stream::StreamExt};
 use serde::Deserialize;
 use std::{sync::Arc, time::Duration};
-use tokio::select;
 use tokio::sync::broadcast::{channel, Sender};
-use tokio::time::sleep;
-use tracing::debug;
+use tokio::{select, time::sleep};
+use tracing::{debug, info_span, trace, Instrument};
 
 const PING_INTERVAL: Duration = Duration::from_secs(20);
 
@@ -52,81 +49,92 @@ async fn ws_handler(
     ws: WebSocketUpgrade,
     state: Extension<State>,
 ) -> impl IntoResponse {
-    debug!("channel {} {:?} connected", channel_id, identity);
-    ws.on_upgrade(move |ws| async move {
-        let mut channel = state.entry(channel_id.clone()).or_insert_with(|| Channel {
-            extension: channel(1).0,
-            website: channel(1).0,
-            num_connections: 0,
-        });
-        channel.num_connections += 1;
+    let span = info_span!("connection", channel_id, ?identity);
 
-        let (sender, mut receiver) = match identity {
-            Identity::Extension => (channel.website.clone(), channel.extension.subscribe()),
-            Identity::Website => (channel.extension.clone(), channel.website.subscribe()),
-        };
-        drop(channel);
+    ws.on_upgrade(move |ws| {
+        async move {
+            debug!("connected");
 
-        let (mut ws_sink, mut ws_stream) = ws.split();
+            let mut channel = state.entry(channel_id.clone()).or_insert_with(|| Channel {
+                extension: channel(1).0,
+                website: channel(1).0,
+                num_connections: 0,
+            });
+            channel.num_connections += 1;
 
-        // Forward outgoing messages and send pings
-        let mut send_task = tokio::spawn(async move {
-            loop {
-                select! {
-                    message = receiver.recv() => {
-                        match message {
-                            Ok(message) => if ws_sink.send(message).await.is_err() {
-                                return;
+            let (sender, mut receiver) = match identity {
+                Identity::Extension => (channel.website.clone(), channel.extension.subscribe()),
+                Identity::Website => (channel.extension.clone(), channel.website.subscribe()),
+            };
+            drop(channel);
+
+            let (mut ws_sink, mut ws_stream) = ws.split();
+
+            // Forward outgoing messages and send pings
+            let mut send_task = tokio::spawn(async move {
+                loop {
+                    select! {
+                        message = receiver.recv() => {
+                            if let Ok(message) = message {
+                                match ws_sink.send(message).await {
+                                    Ok(_) => trace!("sent message"),
+                                    Err(err) => debug!("error sending message to websocket: {err}"),
+                                }
+                            } else {
+                                break;
                             }
-                            Err(_) => break
                         }
-                    }
-                    // Send a ping if no outgoing message has been sent before the timeout
-                    _ = sleep(PING_INTERVAL) => {
-                        if ws_sink.send(Message::Ping(Vec::new())).await.is_err() {
-                            break;
+                        // Send a ping if no outgoing message has been sent before the timeout
+                        _ = sleep(PING_INTERVAL) => {
+                            if ws_sink.send(Message::Ping(Vec::new())).await.is_err() {
+                                break;
+                            }
                         }
                     }
                 }
-            }
-        });
+            });
 
-        // Forward incoming messages
-        let mut receive_task = tokio::spawn(async move {
-            while let Some(Ok(message)) = ws_stream.next().await {
-                match message {
-                    Message::Binary(message) => {
-                        // Ignore send errors because that just means that the other side is not connected
-                        sender.send(Message::Binary(message)).ok();
+            // Forward incoming messages
+            let mut receive_task = tokio::spawn(async move {
+                while let Some(Ok(message)) = ws_stream.next().await {
+                    match message {
+                        Message::Binary(message) => {
+                            // Ignore send errors because that just means that the other side is not connected
+                            sender.send(Message::Binary(message)).ok();
+                        }
+                        // This is only for testing purposes
+                        Message::Text(message) => {
+                            sender.send(Message::Binary(message.into_bytes())).ok();
+                        }
+                        Message::Ping(_) => {
+                            sender.send(Message::Pong(Vec::new())).ok();
+                        }
+                        _ => {}
                     }
-                    // This is only for testing purposes
-                    Message::Text(message) => {
-                        sender.send(Message::Binary(message.into_bytes())).ok();
-                    }
-                    Message::Ping(_) => {
-                        sender.send(Message::Pong(Vec::new())).ok();
-                    }
-                    _ => {}
                 }
+            });
+
+            // If any one of the tasks exit, abort the other.
+            select! {
+                _ = (&mut send_task) => receive_task.abort(),
+                _ = (&mut receive_task) => send_task.abort(),
+                // TODO timeout channels
+            };
+
+            debug!("websocket closed");
+
+            // Remove the channel record when the last connection is dropped
+            let num_channel = {
+                let mut channel = state.get_mut(&channel_id).unwrap();
+                channel.num_connections -= 1;
+                channel.num_connections
+            };
+            if num_channel == 0 {
+                debug!("removing channel");
+                state.remove(&channel_id);
             }
-        });
-
-        // If any one of the tasks exit, abort the other.
-        select! {
-            _ = (&mut send_task) => receive_task.abort(),
-            _ = (&mut receive_task) => send_task.abort(),
-            // TODO timeout channels
-        };
-
-        // Remove the channel record when the last connection is dropped
-        let num_channel = {
-            let mut channel = state.get_mut(&channel_id).unwrap();
-            channel.num_connections -= 1;
-            channel.num_connections
-        };
-        if num_channel == 0 {
-            state.remove(&channel_id);
         }
+        .instrument(span)
     })
 }
 
@@ -134,6 +142,7 @@ async fn delete_channel(
     Path(channel_id): Path<String>,
     state: Extension<State>,
 ) -> impl IntoResponse {
+    debug!("deleting channel {channel_id}");
     state.remove(&channel_id);
 }
 
@@ -147,8 +156,21 @@ async fn post_channel(
             Identity::Extension => channel.website.clone(),
             Identity::Website => channel.extension.clone(),
         };
-        // TODO return error
-        // TODO don't copy the message if possible
-        channel.send(Message::Binary(body.to_vec())).ok();
+
+        match channel.send(Message::Binary(body.to_vec())) {
+            Ok(_) => (StatusCode::OK, ""),
+            Err(err) => {
+                debug!("Error sending message to websocket: {err}");
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "Unable to send message to peer",
+                )
+            }
+        }
+    } else {
+        (
+            StatusCode::NOT_FOUND,
+            "Channel does not exist or has been closed",
+        )
     }
 }
