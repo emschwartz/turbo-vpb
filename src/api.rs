55c6a29b5f3060871f976301e02d5851da3161bc
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2022-09-03 18:53:29 +0200

    Store the extension's first message for backwards compatibility

--- src/api.rs
+++ src/api.rs
@@ -11,6 +11,7 @@ use tokio::{select, time::sleep};
 use tracing::{debug, instrument, trace, Instrument};
 
 const PING_INTERVAL: Duration = Duration::from_secs(20);
+const CHANNEL_CAPACITY: usize = 1;
 
 type State = Arc<DashMap<String, Channel>>;
 struct Channel {
@@ -19,9 +20,25 @@ struct Channel {
     /// Keep track of the number of open channel so we can drop the
     /// channel record when the last connection is dropped.
     num_connections: usize,
+    /// We store the first message sent by the extension for backwards compatibility.
+    /// The extension sends a connect message, which was previously stored by the
+    /// nchan server. Without it, when the website connects, it expects to get a
+    /// connect message but never does, so it never thinks it's connected.
+    extension_first_message: Option<Message>,
 }
 
-#[derive(Deserialize, Debug, Clone, Copy)]
+impl Default for Channel {
+    fn default() -> Self {
+        Self {
+            extension: channel(CHANNEL_CAPACITY).0,
+            browser: channel(CHANNEL_CAPACITY).0,
+            num_connections: 0,
+            extension_first_message: None,
+        }
+    }
+}
+
+#[derive(Deserialize, Debug, Clone, Copy, PartialEq, Eq)]
 #[serde(rename_all = "lowercase")]
 enum Identity {
     Extension,
@@ -58,11 +75,8 @@ async fn ws_handler(
 async fn websocket(channel_id: String, identity: Identity, ws: WebSocket, state: State) {
     debug!("websocket connected");
 
-    let mut channel = state.entry(channel_id.clone()).or_insert_with(|| Channel {
-        extension: channel(1).0,
-        browser: channel(1).0,
-        num_connections: 0,
-    });
+    // Create the channel if it does not already exist
+    let mut channel = state.entry(channel_id.clone()).or_default();
     channel.num_connections += 1;
 
     let (sender, mut receiver) = match identity {
@@ -72,6 +86,17 @@ async fn websocket(channel_id: String, identity: Identity, ws: WebSocket, state:
     drop(channel);
 
     let (mut ws_sink, mut ws_stream) = ws.split();
+
+    // If this is the browser, send the stored first message from the extension
+    if identity == Identity::Browser {
+        if let Some(message) = state
+            .get(&channel_id)
+            .and_then(|channel| channel.extension_first_message.clone())
+        {
+            trace!("sending first message to browser");
+            ws_sink.send(message).await.unwrap();
+        }
+    }
 
     // Forward outgoing messages and send pings
     let mut send_task = tokio::spawn(
@@ -101,13 +126,28 @@ async fn websocket(channel_id: String, identity: Identity, ws: WebSocket, state:
     );
 
     // Forward incoming messages
+    let state_clone = state.clone();
+    let channel_id_clone = channel_id.clone();
     let mut receive_task = tokio::spawn(
         async move {
             while let Some(Ok(message)) = ws_stream.next().await {
                 match message {
-                    Message::Binary(message) => {
+                    Message::Binary(_) => {
+                        // Store the first message from the extension for backwards compatibility
+                        if identity == Identity::Extension
+                            && state_clone
+                                .get(&channel_id_clone)
+                                .map(|c| c.extension_first_message.is_none())
+                                == Some(true)
+                        {
+                            if let Some(mut channel) = state_clone.get_mut(&channel_id_clone) {
+                                trace!("storing first extension message");
+                                channel.extension_first_message = Some(message.clone());
+                            }
+                        }
+
                         // Ignore send errors because that just means that the other side is not connected
-                        if let Err(err) = sender.send(Message::Binary(message)) {
+                        if let Err(err) = sender.send(message) {
                             debug!("error sending message to channel: {err}");
                         }
                     }
